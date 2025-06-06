6c70228b843a4a3f36d042acdd2f85373b76b43f
lovelst988314 <1033211505@qq.com>
2025-06-06 16:31:33 +0800

    work

---
+++ 6.6/a.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    cout << "This is a C++ program using the C++17 standard." << endl;
+    return 0;
+}
